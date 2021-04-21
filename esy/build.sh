dbd5d070b3b7a16714ad1d3de5fa7081e8c61cfd
Zach Baylin <zbaylin@outrunlabs.com>
2021-04-21 15:11:50 -0400

    fix(svg): split out the SVG object files into libskiasvg.a (#29)
    * Build: split out SVG library into separate archive
    
    * Build: copy svg lib

--- esy/build.sh
+++ esy/build.sh
@@ -20,7 +20,7 @@ if [[ $OS == "windows" ]]
 then
     WINDOWS_PYTHON_PATH="$(cygpath -w $(which $PYTHON_BINARY))"
     bin/gn gen $cur__target_dir/out/Shared --script-executable="$WINDOWS_PYTHON_PATH" --args='is_debug=false is_component_build=true' || exit -1
-    ninja.exe -C $cur__target_dir/out/Shared
+    ninja.exe -C $cur__target_dir/out/Shared skia skia.h experimental_svg_model
     mv $cur__target_dir/out/Shared/libskia.dll $cur__target_dir/out/Shared/skia.dll # TODO this might not be required once we merge upstream
     esy/gendef.exe - $cur__target_dir/out/Shared/skia.dll > $cur__target_dir/out/Shared/skia.def
     x86_64-W64-mingw32-dlltool.exe -D $cur__target_dir/out/Shared/skia.dll -d $cur__target_dir/out/Shared/skia.def -A -l $cur__target_dir/out/Shared/libskia.a
@@ -44,6 +44,6 @@ else
     bin/gn gen $cur__target_dir/out/Static --script-executable="$PYTHON_BINARY" "--args=cc=\"$CC\" cxx=\"$CXX\" skia_use_system_libjpeg_turbo=true skia_enable_tools=true is_debug=false extra_cflags=[\"-I${ESY_LIBJPEG_TURBO_PREFIX}/include\"] extra_ldflags=[\"-L${ESY_LIBJPEG_TURBO_PREFIX}/lib\", \"-ljpeg\" ]" || exit -1
     ninja.exe -C $cur__target_dir/out/Static skia skia.h experimental_svg_model || exit -1
     
-    # Add the SVG object files to the Skia archive
-    ar r $cur__target_dir/out/Static/libskia.a $cur__target_dir/out/Static/obj/experimental/svg/model/*.o
+    # Create a new library from the svg object file
+    ar rcs $cur__target_dir/out/Static/libskiasvg.a $cur__target_dir/out/Static/obj/experimental/svg/model/*.o
 fi
